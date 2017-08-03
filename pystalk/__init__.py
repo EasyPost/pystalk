209b65c005b99f9600fc6d5265a690b8fbdfdc0d
James Brown <jbrown@easypost.com>
2017-08-03 13:15:08 -0700

    bump to v0.3.0

--- pystalk/__init__.py
+++ pystalk/__init__.py
@@ -1,6 +1,6 @@
 from .client import BeanstalkClient, BeanstalkError
 
-version_info = (0, 2, 1)
+version_info = (0, 3, 0)
 __version__ = '.'.join(str(s) for s in version_info)
 __author__ = 'EasyPost <oss@easypost.com>'
 
